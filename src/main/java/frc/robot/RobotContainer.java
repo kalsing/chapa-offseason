e068070c715655bc65dddde9882cd1ae5ab41834
kalsing <gabrilel.kalsing@gmail.com>
2025-06-26 09:01:13 -0300

    Exericicio4 incompleto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,13 +1,11 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
@@ -16,40 +14,40 @@ import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class RobotContainer {
-  
-  private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
 
+    private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+    private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
 
-  private final Joystick joystick = new Joystick(0);
-  private final XboxController controller = new XboxController(0); 
+    private final Joystick joystick = new Joystick(0);
+    private final XboxController controller = new XboxController(0);
 
+    public RobotContainer() {
+        configureBindings();
+    }
 
-  public RobotContainer() {
-    configureBindings();
-  }
+    private void configureBindings() {
+        new Trigger(controller::getAButton)
+            .onTrue(Commands.sequence(
+                Commands.run(() -> intakeSubsystem.runIntake(), intakeSubsystem)
+                    .until(() -> intakeSubsystem.isLimitReached())
+                    .andThen(() -> intakeSubsystem.stopIntake()),
 
-  private void configureBindings() {
-    new Trigger(controller::getAButton)
-        .onTrue(Commands.sequence(
-            Commands.run(() -> intakeSubsystem.runIntake(), intakeSubsystem)
-                .until(() -> intakeSubsystem.isLimitReached())
-                .andThen(() -> intakeSubsystem.stopIntake()),
+                Commands.run(() -> shooterSubsystem.runShooter(), shooterSubsystem)
+                    .withTimeout(5),
 
-            Commands.run(() -> shooterSubsystem.runShooter(), shooterSubsystem)
-            .withTimeout(1),
-            new InstantCommand(() -> shooterSubsystem.stopShooter())));
+                new InstantCommand(() -> shooterSubsystem.stopShooter())
+            ));
 
-    new Trigger(controller::getBButton)
-        .onTrue(new InstantCommand(() -> {
-            CommandScheduler.getInstance().cancelAll();
-            shooterSubsystem.stopShooter();
-            intakeSubsystem.stopIntake();}));
+        new Trigger(controller::getBButton)
+            .onTrue(new InstantCommand(() -> {
+                CommandScheduler.getInstance().cancelAll();
+                shooterSubsystem.stopShooter();
+                intakeSubsystem.stopIntake();
+            }));
+
+        new Trigger(controller::getXButton)
+            .onTrue(new InstantCommand(() -> {
+                shooterSubsystem.resetEncoder();
+            }));
+    }
 }
-
-
-
-
-
-
-  }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,21 +1,30 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
- 
-    
-
+   
 SparkMax motorShoot = new SparkMax(1, MotorType.kBrushless);
-private static boolean hasShot = false;
-
+RelativeEncoder encoder = motorShoot.getEncoder();
+double kP = 0.1;
+double kI = 0.0;
+double kD = 0.0;
+PIDController pid = new PIDController(kP, kI, kD);
 public ShooterSubsystem(){}
 
+public void setTargetPosition(double position) {
+   pid.setSetpoint(position);
+}
 
 public void runShooter(){
 motorShoot.set(0.3);
@@ -25,15 +34,20 @@ public void stopShooter(){
     motorShoot.set(0);
 }
 
-   public boolean hasShot(){
-    return hasShot = true;
-   }
-
-   public void resetShot() {
-    hasShot = false;
-   }
-
+public void periodic(){
+   double pos = encoder.getPosition();
+   SmartDashboard.putNumber("EncoderPosicao", pos);
 }
+
+   public boolean atTarget() {
+      return pid.atSetpoint();
+}
+public void resetEncoder() {
+   pid.reset();
+}
+   }
+
+
 
 
 
