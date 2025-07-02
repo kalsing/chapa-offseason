e87861bf0b4ce61163a6a5f087cc04d28c21cfa6
kalsing <gabrilel.kalsing@gmail.com>
2025-07-02 13:14:08 -0300

    PID e controle de RPM (ainda tudo errado)

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.RPM;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 
@@ -16,36 +18,45 @@ public class ShooterSubsystem extends SubsystemBase {
    
 SparkMax motorShoot = new SparkMax(1, MotorType.kBrushless);
 RelativeEncoder encoder = motorShoot.getEncoder();
+double targetRPM = 1000;
+double currentRPM = encoder.getVelocity();
 double kP = 0.1;
 double kI = 0.0;
 double kD = 0.0;
 PIDController pid = new PIDController(kP, kI, kD);
-public ShooterSubsystem(){}
-
-public void setTargetPosition(double position) {
-   pid.setSetpoint(position);
+public ShooterSubsystem(){
+   pid.setSetpoint(targetRPM);
+   double output = pid.calculate(currentRPM);
 }
 
-public void runShooter(){
-motorShoot.set(0.3);
-   }
+   public void runShooter() {
+      double currentRPM = encoder.getVelocity();       // lê o RPM atual
+      double output = pid.calculate(currentRPM);       // calcula a saída PID
+      motorShoot.set(output);                          // aplica ao motor
 
+  }
 public void stopShooter(){
     motorShoot.set(0);
 }
 
 public void periodic(){
    double pos = encoder.getPosition();
+   double RPM = encoder.getVelocity();
    SmartDashboard.putNumber("EncoderPosicao", pos);
+   SmartDashboard.putNumber("RPM", RPM);
+   SmartDashboard.putBoolean("RPM At target?", atTarget());
 }
 
    public boolean atTarget() {
       return pid.atSetpoint();
-}
+  }
+   
+
+
 public void resetEncoder() {
-   pid.reset();
+   encoder.setPosition(0);
 }
-   }
+}
 
 
 
